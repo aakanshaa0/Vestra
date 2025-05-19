80387ca06e7759b0fdec2755b07f89f6dd6c146f
aakanshaa0 <aakanshapande23@gmail.com>
2025-05-19 21:55:26 +0530

    started frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {Route, Routes} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Collection from './pages/Collection';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
+import Orders from './pages/Orders';
+import Product from './pages/Product';
+import PlaceOrder from './pages/PlaceOrder';
+import About from './pages/About';
+import Cart from './pages/Cart';
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path="/collection" element={<Collection/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/orders" element={<Orders/>}/>
+        <Route path="/product/:ProductId" element={<Product/>}/>
+        <Route path="/place-order" element={<PlaceOrder/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import  { useState } from 'react';
+import { IoIosSearch } from "react-icons/io";
+import { IoPersonOutline } from "react-icons/io5";
+import { IoBagOutline } from "react-icons/io5";
+import { IoIosMenu } from "react-icons/io";
+import { MdArrowBackIos } from "react-icons/md";
+
+const Navbar = () => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <div className="flex items-center justify-between py-5 font-medium">
+      <h1 className='text-4xl font-serif cursor-pointer'>Vestra</h1>
+      <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
+        <NavLink to='/' className='flex flex-col items-center gap-1'>
+          <p>HOME</p>
+          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+        </NavLink>
+        <NavLink to='/colletction' className='flex flex-col items-center gap-1'>
+          <p>COLLECTION</p>
+          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+        </NavLink>
+        <NavLink to='/about' className='flex flex-col items-center gap-1'>
+          <p>ABOUT</p>
+          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+        </NavLink>
+        <NavLink to='/contact' className='flex flex-col items-center gap-1'>
+          <p>CONTACT</p>
+          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+        </NavLink>
+      </ul>
+        <div className='flex items-center gap-5'>
+          <IoIosSearch className='h-[25px] w-[25px] cursor-pointer' />
+          <div className='group relative'>
+            <IoPersonOutline className='h-[25px] w-[25px] cursor-pointer' />
+            <div className='hidden group-hover:block absolute dropdown-menu right-0 pt-4'>
+              <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+                <p className='cursor-pointer hover:text-black'>My Profile</p>
+                <p className='cursor-pointer hover:text-black'>Orders</p>
+                <p className='cursor-pointer hover:text-black'>Logout</p>
+              </div>
+            </div>
+          </div>
+          <Link to='/cart' className='relative'>
+            <IoBagOutline className='h-[25px] w-[25px] cursor-pointer' />
+            <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+            <div />
+          </Link>
+          <IoIosMenu onClick={() => setVisible(true)} className='h-[35px] w-[25px] cursor-pointer sm:hidden' />
+        </div>
+        {/*Sidebar Menu for Small screen*/}
+      <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible? 'w-full':'w-0'}`}>
+        <div className='flex flex-col text-gray-600'>
+          <div onClick={()=> setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+            <MdArrowBackIos className='h-[17px] w-[17px] cursor-pointer' />
+            <p>Back</p>
+          </div>
+          <NavLink className='py-2 pl-6 border' to='/'>HOME</NavLink>
+          <NavLink className='py-2 pl-6 border' to='/collection'>COLLECTION</NavLink>
+          <NavLink className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
+          <NavLink className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
